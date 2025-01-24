d032839fd6f2b6931a905daa832b487cd6f53d77
aly <aleebehroozifar1380@gamil.com>
2025-01-24 19:51:59 +0330

    create personel model

--- personnel/models.py
+++ personnel/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Abst(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class personel(Abst):
+
+    BACHELOR = 'bachelor'
+    MASTER = 'master'
+    PHD = 'phd'
+
+    DEGREE_CHOICES = [
+        (BACHELOR, 'Bachelor'),
+        (MASTER, 'Master'),
+        (PHD, 'Phd'),
+    ]
+
+    name = models.CharField(max_length=50)
+    birthdate = models.DateField()
+    fathername = models.CharField(max_length=50)
+    milirary_status = models.BooleanField(default=False)
+    martial_status = models.BooleanField(default=False)
+    degree = models.CharField(choices=DEGREE_CHOICES, max_length=50)
+    employment_data = models.CharField(max_length=100)
